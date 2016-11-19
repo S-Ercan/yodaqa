14ae4f7bfe698e8d5e3bf5a310690113364d173e
Selman Ercan <ercan.s.selman@gmail.com>
2016-11-19 16:11:53 +0100

    PassFilter creates PickedPassagesInfo feature structures containing the number of collected passages.

--- src/main/java/cz/brmlab/yodaqa/analysis/passextract/PassFilter.java
+++ src/main/java/cz/brmlab/yodaqa/analysis/passextract/PassFilter.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import cz.brmlab.yodaqa.flow.dashboard.QuestionDashboard;
 import cz.brmlab.yodaqa.flow.dashboard.snippet.AnsweringPassage;
 import cz.brmlab.yodaqa.flow.dashboard.snippet.SnippetIDGenerator;
+import cz.brmlab.yodaqa.model.PickedPassage.PickedPassageInfo;
 import cz.brmlab.yodaqa.model.SearchResult.ResultInfo;
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
@@ -26,83 +27,95 @@ import org.slf4j.LoggerFactory;
 import cz.brmlab.yodaqa.model.SearchResult.Passage;
 
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.impl.FeatureStructureImpl;
+import org.apache.uima.cas.impl.FeatureStructureImplC;
 
 /**
- * Generate Passages from Sentences that contain some Clue in Question
- * and copy them over to the Passages view.
+ * Generate Passages from Sentences that contain some Clue in Question and copy
+ * them over to the Passages view.
  *
- * Prospectively, we might want to keep some surrounding sentences,
- * though. */
-
+ * Prospectively, we might want to keep some surrounding sentences, though.
+ */
 @SofaCapability(
-	inputSofas = { "Passages" },
-	outputSofas = { "PickedPassages" }
+        inputSofas = {"Passages"},
+        outputSofas = {"PickedPassages"}
 )
 
-
 public class PassFilter extends JCasAnnotator_ImplBase {
-	final Logger logger = LoggerFactory.getLogger(PassFilter.class);
 
-	/** Number of passages to pick for detailed analysis.
-	 * Keep in sync with the data/ml/passextract-train.py num_picked
-	 * variable. */
-	public static final String PARAM_NUM_PICKED = "num-picked";
-	@ConfigurationParameter(name = PARAM_NUM_PICKED, mandatory = false, defaultValue = "3")
-	private int numPicked;
+    final Logger logger = LoggerFactory.getLogger(PassFilter.class);
 
-	public void initialize(UimaContext aContext) throws ResourceInitializationException {
-		super.initialize(aContext);
-	}
+    /**
+     * Number of passages to pick for detailed analysis. Keep in sync with the
+     * data/ml/passextract-train.py num_picked variable.
+     */
+    public static final String PARAM_NUM_PICKED = "num-picked";
+    @ConfigurationParameter(name = PARAM_NUM_PICKED, mandatory = false, defaultValue = "3")
+    private int numPicked;
 
-	public void process(JCas jcas) throws AnalysisEngineProcessException {
-		JCas resultView, passagesView, pickedPassagesView,questionView;
-		try {
-			resultView = jcas.getView("Result");
-			passagesView = jcas.getView("Passages");
-			jcas.createView("PickedPassages");
-			questionView = jcas.getView("Question");
-			pickedPassagesView = jcas.getView("PickedPassages");
-		} catch (CASException e) {
-			throw new AnalysisEngineProcessException(e);
-		}
-		pickedPassagesView.setDocumentText(passagesView.getDocumentText());
-		pickedPassagesView.setDocumentLanguage(passagesView.getDocumentLanguage());
+    public void initialize(UimaContext aContext) throws ResourceInitializationException {
+        super.initialize(aContext);
+    }
 
-		int sourceID = JCasUtil.selectSingle(resultView, ResultInfo.class).getSourceID();
+    public void process(JCas jcas) throws AnalysisEngineProcessException {
+        JCas resultView, passagesView, pickedPassagesView, questionView;
+        try {
+            resultView = jcas.getView("Result");
+            passagesView = jcas.getView("Passages");
+            jcas.createView("PickedPassages");
+            questionView = jcas.getView("Question");
+            pickedPassagesView = jcas.getView("PickedPassages");
+        } catch (CASException e) {
+            throw new AnalysisEngineProcessException(e);
+        }
+        pickedPassagesView.setDocumentText(passagesView.getDocumentText());
+        pickedPassagesView.setDocumentLanguage(passagesView.getDocumentLanguage());
 
-		/* Pre-index covering info. */
-		Map<Passage, Collection<Annotation>> covering =
-			JCasUtil.indexCovered(passagesView, Passage.class, Annotation.class);
+        int sourceID = JCasUtil.selectSingle(resultView, ResultInfo.class).getSourceID();
 
-		/* Just copy over the first N ones. */
-		FSIndex idx = passagesView.getJFSIndexRepository().getIndex("SortedPassages");
-		FSIterator passages = idx.iterator();
-		int i = 0;
-		CasCopier copier = new CasCopier(passagesView.getCas(), pickedPassagesView.getCas());
-		while (passages.hasNext() && i++ < numPicked) {
-			Passage passage = (Passage) passages.next();
-			AnsweringPassage ap = new AnsweringPassage(SnippetIDGenerator.getInstance().generateID(), sourceID, passage.getCoveredText());
-			QuestionDashboard.getInstance().get(questionView).addSnippet(ap);
+        /* Pre-index covering info. */
+        Map<Passage, Collection<Annotation>> covering
+                = JCasUtil.indexCovered(passagesView, Passage.class, Annotation.class);
 
-			Passage p2 = (Passage) copier.copyFs(passage);
-			p2.setSnippetID(ap.getSnippetID());
-			p2.addToIndexes();
+        /* Just copy over the first N ones. */
+        FSIndex idx = passagesView.getJFSIndexRepository().getIndex("SortedPassages");
+        FSIterator passages = idx.iterator();
+        int i = 0;
+        CasCopier copier = new CasCopier(passagesView.getCas(), pickedPassagesView.getCas());
+        List<Passage> passagesList = new ArrayList<>();
+        while (passages.hasNext() && i++ < numPicked) {
+            Passage passage = (Passage) passages.next();
+            passagesList.add(passage);
+            AnsweringPassage ap = new AnsweringPassage(SnippetIDGenerator.getInstance().generateID(), sourceID, passage.getCoveredText());
+            QuestionDashboard.getInstance().get(questionView).addSnippet(ap);
 
-			/* Also recursively copy annotations - we need
+            Passage p2 = (Passage) copier.copyFs(passage);
+            p2.setSnippetID(ap.getSnippetID());
+            p2.addToIndexes();
+
+            /* Also recursively copy annotations - we need
 			 * to have Sentences in the PickedPassages view
 			 * to run a parser. */
-			for (Annotation a : covering.get(passage)) {
-				if (!copier.alreadyCopied(a)) {
-					Annotation a2 = (Annotation) copier.copyFs(a);
-					a2.addToIndexes();
-				}
-			}
+            for (Annotation a : covering.get(passage)) {
+                if (!copier.alreadyCopied(a)) {
+                    Annotation a2 = (Annotation) copier.copyFs(a);
+                    a2.addToIndexes();
+                }
+            }
 
-			/* Count tokens, just for a debug print.
+            /* Count tokens, just for a debug print.
 			 * This is relevant because of StanfordParser
 			 * MAX_TOKENS limit. */
-			int n_tokens = JCasUtil.selectCovered(Token.class, passage).size();
-			logger.debug(passage.getScore() + " | " + passage.getCoveredText() + " | " + n_tokens);
-		}
-	}
+            int n_tokens = JCasUtil.selectCovered(Token.class, passage).size();
+            logger.debug(passage.getScore() + " | " + passage.getCoveredText() + " | " + n_tokens);
+        }
+
+        PickedPassageInfo ppInfo;
+        ppInfo = new PickedPassageInfo(pickedPassagesView);
+        ppInfo.setNumPassages(ppInfo.getNumPassages() + Math.min(i, numPicked));
+        ppInfo.addToIndexes(pickedPassagesView);
+    }
 }
--- src/main/java/cz/brmlab/yodaqa/analysis/question/AlpinoParser.java
+++ src/main/java/cz/brmlab/yodaqa/analysis/question/AlpinoParser.java
@@ -1,5 +1,7 @@
 package cz.brmlab.yodaqa.analysis.question;
 
+import cz.brmlab.yodaqa.model.PickedPassage.PickedPassageInfo;
+import cz.brmlab.yodaqa.model.SearchResult.Passage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -33,140 +35,148 @@ import org.xml.sax.SAXParseException;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.dependency.Dependency;
+import java.util.Iterator;
 
 public class AlpinoParser extends JCasAnnotator_ImplBase {
 
-	private String hostName = "localhost";
-	private int parsePortNumber = 42424;
+    private String hostName = "localhost";
+    private int parsePortNumber = 42424;
 
-	private Socket parseSocket;
-	private PrintWriter parseOut;
-	private BufferedReader parseIn;
+    private Socket parseSocket;
+    private PrintWriter parseOut;
+    private BufferedReader parseIn;
 
-	public void initialize(UimaContext aContext) throws ResourceInitializationException {
-		super.initialize(aContext);
-	}
+    public void initialize(UimaContext aContext) throws ResourceInitializationException {
+        super.initialize(aContext);
+    }
 
-	@Override
-	public void process(JCas aJCas) throws AnalysisEngineProcessException {
+    @Override
+    public void process(JCas aJCas) throws AnalysisEngineProcessException {
 
-		FSIterator<Annotation> typeToParseIterator = aJCas.getAnnotationIndex(JCasUtil.getType(aJCas, Sentence.class))
-				.iterator();
+        Iterator<PickedPassageInfo> ppInfoIterator = JCasUtil.select(aJCas, PickedPassageInfo.class).iterator();
+        int numPassages = 0;
+        while (ppInfoIterator.hasNext()) {
+            numPassages += ppInfoIterator.next().getNumPassages();
+        }
+        System.out.println("Number of passages: " + numPassages);
 
-		while (typeToParseIterator.hasNext()) {
-			try {
-				// TODO: initializing this once (which should be done) leads to
-				// problems in case typeToParseIterator does have multiple
-				// entries; figure out a fix so we don't have to reinitialize
-				// this on every iteration
-				parseSocket = new Socket(hostName, parsePortNumber);
-				parseOut = new PrintWriter(parseSocket.getOutputStream(), true);
-				parseIn = new BufferedReader(new InputStreamReader(parseSocket.getInputStream()));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+        FSIterator<Annotation> typeToParseIterator = aJCas.getAnnotationIndex(JCasUtil.getType(aJCas, Sentence.class))
+                .iterator();
 
-			List<Token> tokenList = new ArrayList<Token>();
-			Annotation currAnnotationToParse = typeToParseIterator.next();
-			// Combine tokens into sentence
-			String sentence = "";
-			String text;
-			for (Token token : JCasUtil.selectCovered(Token.class, currAnnotationToParse)) {
-				tokenList.add(token);
-				text = token.getCoveredText();
-				text = Normalizer.normalize(text, Normalizer.Form.NFD);
-				sentence += text + ' ';
-			}
-			if (sentence.equals("")) {
-				continue;
-			}
-			// Get parse tree and dependency triples
-			String parseOutput = getParseOutput(sentence);
-			Document parseTree = processParseTree(parseOutput);
-			if (parseTree == null) {
-				System.out.println("No parse tree for the following sentence was received: " + sentence);
-				continue;
-			}
-			annotateConstituents(tokenList, parseTree.getDocumentElement());
-			annotateDependencies(aJCas, sentence, tokenList);
-			try {
-				JCas ppView = aJCas.getView("PickedPassages");
-			} catch (CASRuntimeException e) {
+        while (typeToParseIterator.hasNext()) {
+            try {
+                // TODO: initializing this once (which should be done) leads to
+                // problems in case typeToParseIterator does have multiple
+                // entries; figure out a fix so we don't have to reinitialize
+                // this on every iteration
+                parseSocket = new Socket(hostName, parsePortNumber);
+                parseOut = new PrintWriter(parseSocket.getOutputStream(), true);
+                parseIn = new BufferedReader(new InputStreamReader(parseSocket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            List<Token> tokenList = new ArrayList<Token>();
+            Annotation currAnnotationToParse = typeToParseIterator.next();
+            // Combine tokens into sentence
+            String sentence = "";
+            String text;
+            for (Token token : JCasUtil.selectCovered(Token.class, currAnnotationToParse)) {
+                tokenList.add(token);
+                text = token.getCoveredText();
+                text = Normalizer.normalize(text, Normalizer.Form.NFD);
+                sentence += text + ' ';
+            }
+            if (sentence.equals("")) {
+                continue;
+            }
+            // Get parse tree and dependency triples
+            String parseOutput = getParseOutput(sentence);
+            Document parseTree = processParseTree(parseOutput);
+            if (parseTree == null) {
+                System.out.println("No parse tree for the following sentence was received: " + sentence);
+                continue;
+            }
+            annotateConstituents(tokenList, parseTree.getDocumentElement());
+            annotateDependencies(aJCas, sentence, tokenList);
+            try {
+                JCas ppView = aJCas.getView("PickedPassages");
+            } catch (CASRuntimeException e) {
 //				List<Dependency> dependencies = annotateDependencies(aJCas, sentence, tokenList);
-			} catch (CASException e) {
-				e.printStackTrace();
-			}
-		}
-		if (parseSocket != null) {
-			try {
-				parseSocket.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+            } catch (CASException e) {
+                e.printStackTrace();
+            }
+        }
+        if (parseSocket != null) {
+            try {
+                parseSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	private void annotateConstituents(List<Token> tokenList, Node treeNode) {
-		AlpinoConstituentAnnotator annotator;
-		Annotation annotation = null;
-		try {
-			annotator = new AlpinoConstituentAnnotator(tokenList);
-			annotation = annotator.createConstituentAnnotationFromTree(treeNode, null, true);
-		} catch (CASException e) {
-			e.printStackTrace();
-		}
-	}
+    private void annotateConstituents(List<Token> tokenList, Node treeNode) {
+        AlpinoConstituentAnnotator annotator;
+        Annotation annotation = null;
+        try {
+            annotator = new AlpinoConstituentAnnotator(tokenList);
+            annotation = annotator.createConstituentAnnotationFromTree(treeNode, null, true);
+        } catch (CASException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private void annotateDependencies(JCas aJCas, String sentence, List<Token> tokenList) {
-		AlpinoDependencyAnnotator depAnnotator;
-		List<Dependency> dependencies = null;
-		try {
-			depAnnotator = new AlpinoDependencyAnnotator(tokenList);
-			String dependencyOutput;
-			try {
-				dependencyOutput = depAnnotator.getDependencyOutput(sentence);
-				dependencies = depAnnotator.processDependencyTriples(aJCas, dependencyOutput);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} catch (CASException e) {
-			e.printStackTrace();
-		}
-	}
+    private void annotateDependencies(JCas aJCas, String sentence, List<Token> tokenList) {
+        AlpinoDependencyAnnotator depAnnotator;
+        List<Dependency> dependencies = null;
+        try {
+            depAnnotator = new AlpinoDependencyAnnotator(tokenList);
+            String dependencyOutput;
+            try {
+                dependencyOutput = depAnnotator.getDependencyOutput(sentence);
+                dependencies = depAnnotator.processDependencyTriples(aJCas, dependencyOutput);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (CASException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private String getParseOutput(String sentence) {
-            // TODO: investigate passing all sentences off at once during answer analysis.
-		parseOut.println(sentence);
-		String line;
-		String output = "";
-		try {
-			while ((line = parseIn.readLine()) != null) {
-				output += line;
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return output;
-	}
+    private String getParseOutput(String sentence) {
+        // TODO: investigate passing all sentences off at once during answer analysis.
+        parseOut.println(sentence);
+        String line;
+        String output = "";
+        try {
+            while ((line = parseIn.readLine()) != null) {
+                output += line;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output;
+    }
 
-	private Document processParseTree(String parseOutput) {
-		Document parseTree = null;
-		try {
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			parseTree = dBuilder.parse(new InputSource(new StringReader(parseOutput)));
-			parseTree.getDocumentElement().normalize();
-			return parseTree;
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		} catch (SAXParseException e) {
-			System.err.println("Could not parse string.");
-		} catch (SAXException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return parseTree;
-	}
+    private Document processParseTree(String parseOutput) {
+        Document parseTree = null;
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            parseTree = dBuilder.parse(new InputSource(new StringReader(parseOutput)));
+            parseTree.getDocumentElement().normalize();
+            return parseTree;
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXParseException e) {
+            System.err.println("Could not parse string.");
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return parseTree;
+    }
 
 }
